4c81a9e50f9f31132fc9cea037c3c15907724c14
Sashen <SGovindasamy@dvtsoftware.com>
2026-02-22 22:28:47 +0200

    Updated types and data to include drinks data and items

--- src/types.ts
+++ src/types.ts
@@ -9,4 +9,21 @@ export type MenuItem = {
   price: number;
   description: string;
   spicy: boolean;
+  image: string;
+};
+
+// adding controls for the menu item 
+export type SortOrder = 'none' | 'low-high' | 'high-low';
+
+// drinks category
+export type DrinkCategory = 'Cold' | 'Hot' | 'Alcoholic' | 'Smoothie';
+
+// drink item type 
+export type DrinkItem = {
+  id: number;
+  name: string;
+  category: DrinkCategory;
+  price: number;
+  description: string;
+  image: string;
 };
